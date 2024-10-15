8774504574db1f0dc528db652199f2048da457cb
Hali420 <ahmed.hali@esprit.tn>
2024-10-15 16:10:25 +0200

    fixed

---
+++ xx/js/index.js
@@ -0,0 +1,27 @@
+function toggleMenu() {
+    var content = document.getElementById("menuContent");
+    if (content.style.display === "block") {
+        content.style.display = "none";
+    } else {
+        content.style.display = "block";
+    }
+}
+
+function toggleFooterLeft() {
+    var content = document.getElementById("footLeftContent");
+    if (content.style.display === "block") {
+        content.style.display = "none";
+    } else {
+        content.style.display = "block";
+    }
+}
+
+
+function toggleFooterRight() {
+    var content = document.getElementById("footRightContent");
+    if (content.style.display === "block") {
+        content.style.display = "none";
+    } else {
+        content.style.display = "block";
+    }
+}
